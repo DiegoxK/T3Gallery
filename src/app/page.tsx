0ba15d727aa49857538ec2d2608d5ab9bbcbd13b
DiegoxK <diego.stv@outlook.es>
2024-04-18 17:29:13 -0500

    Updated eslint configuration and layout styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+const mockUrls = [
+  "https://utfs.io/f/02c6d10c-6f88-4d91-b490-48d1d6ed16f7-jk7jec.webp",
+  "https://utfs.io/f/003eafa7-8e9c-4e65-a6d9-8996afb70091-4jltdg.webp",
+];
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello (gallery in progress)
+    <main>
+      <div className="flex flex-wrap gap-8">
+        {mockUrls.map((url) => (
+          <img src={url} alt="mock" className="w-48" />
+        ))}
+      </div>
     </main>
   );
 }
