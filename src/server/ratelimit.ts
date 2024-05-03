63b4dfce4b9ad551081e7679f84502793a07046a
DiegoxK <diego.stv@outlook.es>
2024-05-03 09:36:37 -0500

    Update ratelimit configuration to allow 5 requests per 100 seconds

--- src/server/ratelimit.ts
+++ src/server/ratelimit.ts
@@ -4,7 +4,7 @@ import { Redis } from "@upstash/redis";
 // Create a new ratelimiter, that allows 10 requests per 10 seconds
 export const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
-  limiter: Ratelimit.slidingWindow(2, "100 s"),
+  limiter: Ratelimit.slidingWindow(5, "100 s"),
   analytics: true,
   prefix: "@upstash/ratelimit",
 });
